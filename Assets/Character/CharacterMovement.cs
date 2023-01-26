f4a6d148a788983eed0623649c53aca137ff3d26
EgorDubovik <posik.web.m@gmail.com>
2023-01-26 13:38:45 -0600

    fixes

--- Assets/Character/CharacterMovement.cs
+++ Assets/Character/CharacterMovement.cs
@@ -6,10 +6,12 @@ public class CharacterMovement : MonoBehaviour
 {
 	public float moveSpeed = 5f;
 	public CharacterController controller;
+	public Animator animator;
 
 	void Start()
 	{
       controller = GetComponent<CharacterController>();
+      animator = GetComponent<Animator>();
    }
         
   void Update()
@@ -19,5 +21,17 @@ public class CharacterMovement : MonoBehaviour
 
 		Vector3 move = transform.right * x + transform.forward * z;
 		controller.Move(move * moveSpeed * Time.deltaTime);
+
+		if(Input.anyKey){
+			if(Input.GetKey(KeyCode.W)) animator.SetBool("IsMovingF",true); 
+			if(Input.GetKey(KeyCode.S)) animator.SetBool("IsMovingB",true); 
+			if(Input.GetKey(KeyCode.A)) animator.SetBool("IsMovingL",true); 
+			if(Input.GetKey(KeyCode.D)) animator.SetBool("IsMovingR",true); 
+			if(Input.GetMouseButton(0)) animator.SetBool("IsShoot",true); 
+		}
+		if(Input.GetKeyUp(KeyCode.W)) animator.SetBool("IsMovingF",false); 
+		if(Input.GetKeyUp(KeyCode.S)) animator.SetBool("IsMovingB",false);
+		if(Input.GetKeyUp(KeyCode.A)) animator.SetBool("IsMovingL",false);
+		if(Input.GetKeyUp(KeyCode.D)) animator.SetBool("IsMovingR",false);
    }
 }
--- Assets/Character/Rotation.cs
+++ Assets/Character/Rotation.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class Rotation : MonoBehaviour
 {
-	public Animator animator;
+	
 	void Start()
 	{
-		animator = GetComponent<Animator>();
+		
 	}
 
 	// Update is called once per frame
@@ -20,16 +20,6 @@ public class Rotation : MonoBehaviour
 			transform.LookAt(target);
 		}
 
-		if(Input.anyKey){
-			if(Input.GetKey(KeyCode.W)) animator.SetBool("IsMovingF",true); 
-			if(Input.GetKey(KeyCode.S)) animator.SetBool("IsMovingB",true); 
-			if(Input.GetKey(KeyCode.A)) animator.SetBool("IsMovingL",true); 
-			if(Input.GetKey(KeyCode.D)) animator.SetBool("IsMovingR",true); 
-			if(Input.GetMouseButton(0)) animator.SetBool("IsShoot",true); 
-		}
-		if(Input.GetKeyUp(KeyCode.W)) animator.SetBool("IsMovingF",false); 
-		if(Input.GetKeyUp(KeyCode.S)) animator.SetBool("IsMovingB",false);
-		if(Input.GetKeyUp(KeyCode.A)) animator.SetBool("IsMovingL",false);
-		if(Input.GetKeyUp(KeyCode.D)) animator.SetBool("IsMovingR",false);
+		
 	}
 }
