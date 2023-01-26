c57d9b7736dae82c0f6558d275c027b3a6bb160d
EgorDubovik <posik.web.m@gmail.com>
2023-01-26 07:52:54 -0600

    Movment

--- Assets/Character/CharacterMovement.cs
+++ Assets/Character/CharacterMovement.cs
@@ -7,26 +7,51 @@ public class CharacterMovement : MonoBehaviour
 	public float moveSpeed = 5f;
 	public float rotationSpeed = 5f;
 	public CharacterController controller;
-	
+	public Animator animator;
+	Vector3 oldPos;
+	Quaternion oldRot;
+
 	void Start()
 	{
-        controller = GetComponent<CharacterController>();
-    }
+		animator = GetComponent<Animator>();
+      controller = GetComponent<CharacterController>();
+
+      oldPos = transform.position;
+    	oldRot = transform.rotation;
+   }
         
-     void Update()
-     {
-        float x = Input.GetAxis("Horizontal");
-        float z = Input.GetAxis("Vertical");
+  void Update()
+  {
+		float x = Input.GetAxis("Horizontal");
+		float z = Input.GetAxis("Vertical");
 
-        Vector3 move = transform.right * x + transform.forward * z;
+		Vector3 move = transform.right * x + transform.forward * z;
+		controller.Move(move * moveSpeed * Time.deltaTime);
 
-        controller.Move(move * moveSpeed * Time.deltaTime);
 
-     /*   Vector3 cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        cursorPos.y = transform.position.y;
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		if(Physics.Raycast(ray, out RaycastHit hitInfo, maxDistance: 300f)){
+			var target = hitInfo.point;
+			target.y = transform.position.y;
+			transform.LookAt(target);
+		}
 
-        Vector3 lookDirection = cursorPos - transform.position;
-        Quaternion rotation = Quaternion.LookRotation(lookDirection);
-        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, rotationSpeed * Time.deltaTime);
-     */ }
+		if(Input.GetKey(KeyCode.W)) animator.SetBool("IsMovingF",true); else animator.SetBool("IsMovingF",false);
+		if(Input.GetKey(KeyCode.S)) animator.SetBool("IsMovingB",true); else animator.SetBool("IsMovingB",false);
+		if(Input.GetKey(KeyCode.A)) animator.SetBool("IsMovingL",true); else animator.SetBool("IsMovingL",false);
+		if(Input.GetKey(KeyCode.D)) animator.SetBool("IsMovingR",true); else animator.SetBool("IsMovingR",false);
+		if(Input.GetMouseButton(0)) animator.SetBool("IsShoot",true); else animator.SetBool("IsShoot",false);
+
+
+		
+
+     //Camera.main.transform.position.y
+   //   Vector3 cursorPos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z));
+   //   Debug.Log(cursorPos);
+	  // cursorPos.y = transform.position.y;
+	  // Vector3 lookDirection = cursorPos - transform.position;
+	  // Quaternion rotation = Quaternion.LookRotation(lookDirection);
+	  // transform.rotation = Quaternion.Slerp(transform.rotation, rotation, rotationSpeed * Time.deltaTime);
+
+   }
 }
