8ea3d5f4f622b57bfb8e9eee7654152cc5b8eddf
EgorDubovik <posik.web.m@gmail.com>
2023-01-26 11:10:46 -0600

    movment

--- Assets/Character/CharacterMovement.cs
+++ Assets/Character/CharacterMovement.cs
@@ -5,19 +5,11 @@ using UnityEngine;
 public class CharacterMovement : MonoBehaviour
 {
 	public float moveSpeed = 5f;
-	public float rotationSpeed = 5f;
 	public CharacterController controller;
-	public Animator animator;
-	Vector3 oldPos;
-	Quaternion oldRot;
 
 	void Start()
 	{
-		animator = GetComponent<Animator>();
       controller = GetComponent<CharacterController>();
-
-      oldPos = transform.position;
-    	oldRot = transform.rotation;
    }
         
   void Update()
@@ -27,31 +19,5 @@ public class CharacterMovement : MonoBehaviour
 
 		Vector3 move = transform.right * x + transform.forward * z;
 		controller.Move(move * moveSpeed * Time.deltaTime);
-
-
-		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-		if(Physics.Raycast(ray, out RaycastHit hitInfo, maxDistance: 300f)){
-			var target = hitInfo.point;
-			target.y = transform.position.y;
-			transform.LookAt(target);
-		}
-
-		if(Input.GetKey(KeyCode.W)) animator.SetBool("IsMovingF",true); else animator.SetBool("IsMovingF",false);
-		if(Input.GetKey(KeyCode.S)) animator.SetBool("IsMovingB",true); else animator.SetBool("IsMovingB",false);
-		if(Input.GetKey(KeyCode.A)) animator.SetBool("IsMovingL",true); else animator.SetBool("IsMovingL",false);
-		if(Input.GetKey(KeyCode.D)) animator.SetBool("IsMovingR",true); else animator.SetBool("IsMovingR",false);
-		if(Input.GetMouseButton(0)) animator.SetBool("IsShoot",true); else animator.SetBool("IsShoot",false);
-
-
-		
-
-     //Camera.main.transform.position.y
-   //   Vector3 cursorPos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z));
-   //   Debug.Log(cursorPos);
-	  // cursorPos.y = transform.position.y;
-	  // Vector3 lookDirection = cursorPos - transform.position;
-	  // Quaternion rotation = Quaternion.LookRotation(lookDirection);
-	  // transform.rotation = Quaternion.Slerp(transform.rotation, rotation, rotationSpeed * Time.deltaTime);
-
    }
 }
