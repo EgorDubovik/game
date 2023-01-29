6428b4c9e60e2ea502779e5a59ced685026776f0
EgorDubovik <posik.web.m@gmail.com>
2023-01-29 08:31:06 -0600

    gun pickUp

--- Assets/Scripts/TriggerController.cs
+++ Assets/Scripts/TriggerController.cs
@@ -7,7 +7,7 @@ public class TriggerController : MonoBehaviour
 	public SkinnedMeshRenderer body;
 	public Mesh meshBody;
 	public void OnTriggerEnter(Collider other){
-
+		
 		body.sharedMesh = meshBody;
 	}
 }
--- Assets/Scripts/WeaponTriger.cs
+++ Assets/Scripts/WeaponTriger.cs
@@ -7,26 +7,39 @@ public class WeaponTriger : MonoBehaviour
 
 
 	public GameObject self;
-	public WeaponAssets weapons;
-	private GameObject person;
 
+	public void Start(){
+		
+	}
    public void OnTriggerEnter(Collider other){
-   	GameObject newWeapon = FindWeaponByName(self.name);
+   	Transform t = other.gameObject.transform.Find("BasicMaleDefault/root/pelvis/spine_01/spine_02/spine_03/clavicle_r/upperarm_r/lowerarm_r/hand_r");
+
+   	GameObject newWeapon = FindWeaponByName(self.name,t);
+
+   	
 
    	if(newWeapon!=null){
-
    		newWeapon.SetActive(true);
+   	
+   	} else {
+   		Debug.Log("NUll");
    	}
    }
 
 
-   private GameObject FindWeaponByName(string name){
-   	foreach (GameObject weapon in weapons.weapons){
-   		weapon.SetActive(false);
-   		if(name.Equals(weapon.name)){
-   			return weapon;
-   		}
+   private GameObject FindWeaponByName(string name, Transform hand){
+
+   	GameObject newWeapon = null;
+
+   	foreach (Transform weapon in hand){
+   		if(weapon.tag.Equals("gun")){
+   			weapon.gameObject.SetActive(false);
+   			Debug.Log($"{weapon.name} SetActive false");
+	   		if(name.Equals(weapon.name)){
+	   			newWeapon = weapon.gameObject;
+	   		}
+	   	}
    	}
-   	return null;
+   	return newWeapon;
    }
 }
